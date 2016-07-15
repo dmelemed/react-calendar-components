39a1b40876fb01a4ff30892b445afe5ae2e9f72c
Daniel Melemed <dmelemed@Daniels-MacBook-Pro.local>
2016-07-15 17:22:18 -0400

    Fixed month cell div inside tr should be td

--- modules/MonthCell.js
+++ modules/MonthCell.js
@@ -4,19 +4,22 @@ import classNames from 'classnames';
 
 export default function MonthCell(props) {
 
-    const { day, today, monthIndex, style, eventStyle, events} = props;
+    const {day, today, monthIndex, style, eventStyle, events} = props;
 
     let eventBoxes = events.map((event, eventIndex) => {
         let eventTime = moment(event.date).format('h:mm a')
         return (
-            <div key={eventIndex}
-                 style={eventStyle}>{eventTime} {event.title ? event.title : '(No title)'}</div>
+            <tr key={eventIndex}>
+                <td
+                    style={eventStyle}>{eventTime} {event.title ? event.title : '(No title)'}</td>
+            </tr>
+
         );
     });
 
     let dayBoxesClasses = classNames({
         calendarBox: true,
-        monthViewDayCell : true,
+        monthViewDayCell: true,
         todayCalendarBox: today && day.isSame(today, 'day')
     });
 
@@ -29,9 +32,7 @@ export default function MonthCell(props) {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    {eventBoxes}
-                </tr>
+                {eventBoxes}
                 </tbody>
             </table>
         </td>
